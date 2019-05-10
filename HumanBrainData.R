2826290c6d7ef8f7a5cb884a57ab64eb9953f9ef
MMDarby <43556758+MMDarby@users.noreply.github.com>
2019-05-10 15:03:20 -0400

    identify SRA Studies for Brain Regions

--- HumanBrainData.R
+++ HumanBrainData.R
@@ -5,7 +5,6 @@
 # Recount2 paper: https://www.biorxiv.org/content/10.1101/247346v2
 # Recount2 published workflow: https://f1000research.com/articles/6-1558/v1
 # Recount2 website: https://jhubiostatistics.shinyapps.io/recount/
-# SRA entry for downloaded dataset: https://trace.ncbi.nlm.nih.gov/Traces/sra/?study=SRP051688
 # Recount-brain paper for future use: https://www.biorxiv.org/content/10.1101/618025v1
 ###########################################
 
@@ -18,14 +17,42 @@
 
 library(recount)
 
+unique(brainStudies$tissue_site_1)
+# [1] "Cerebral cortex"    NA                   "Mixed"              "Whole brain"       
+# [5] "Cerebellum"         "Brainstem"          "Corpus callosum"    "Nucleus accumbens" 
+# [9] "Dura mater"         "Putamen"            "Substantia nigra"   "Hippocampus"       
+# [13] "Lumbar spinal cord" "Frontal Cortex"     "Caudate" 
+
 # to add brain specific metadata to Recount2 
 brainStudies <- add_metadata()
 ## Subset out studies of interest
+
 #### Find studies that include samples for Nucelus Accumbens, Putamen, Substantia Nigra, Hippocampus, Frontal Cortex, Caudate
-idxT1 <- which(brainStudies$tissue_site_1=="Nucelus accumbens") | which(brainStudies$tissue_site_1=="Putamen") | which(brainStudies$tissue_site_1=="Substantia nigra") | which(brainStudies$tissue_site_1=="Hippocampus") | which(brainStudies$tissue_site_1=="Frontal Cortex")| which(brainStudies$tissue_site_1=="Caudate")
+NucAcc <- which(brainStudies[, "tissue_site_1"] == "Nucleus accumbens")
+Put <- which(brainStudies$tissue_site_1=="Putamen")
+SubNig <- which(brainStudies$tissue_site_1=="Substantia nigra")
+Hip <- which(brainStudies$tissue_site_1=="Hippocampus")
+FC <- which(brainStudies$tissue_site_1=="Frontal Cortex")
+Caud <- which(brainStudies$tissue_site_1=="Caudate")
 
 #### Find studies that include samples for dorsolateral prefrontal cortex, prefontal cortex
-idxT2 <- which(brainStudies$tissue_site_3=="Dorsolateral prefrontal cortex") | which(brainStudies$tissue_site_3=="Prefontal cortex") 
+unique(brainStudies$tissue_site_3)
+# [1] "Superior temporal gyrus"       
+# [2] "Dorsolateral prefrontal cortex"
+# [3] NA                              
+# [4] "Motor cortex"                  
+# [5] "Superior frontal gyrus"        
+# [6] "Anterior cingulate gyrus"      
+# [7] "Somatosensory cortex"          
+# [8] "Prefrontal cortex"             
+# [9] "Anterior prefrontal cortex"    
+DPC <-which(brainStudies$tissue_site_3=="Dorsolateral prefrontal cortex")
+PC <- which(brainStudies$tissue_site_3=="Prefrontal cortex")
+
+AllRegions <- c(NucAcc, Put, SubNig, Hip, FC, Caud, DPC, PC)
+(AllRegions <- unique(AllRegions))
+(BestRegions <- c(NucAcc, SubNig, Hip, FC))
+
 #### Print info on workspace environment
 sessionInfo()
 
