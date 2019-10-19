39ead3413d9e805a28176c21b2dd801cd92286c7
owentsai <31111304+owentsai@users.noreply.github.com>
2019-10-19 14:58:51 -0700

    change pathname

--- scripts/fetch_human_protein_coding_gene.R
+++ scripts/fetch_human_protein_coding_gene.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 download.file("ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2ensembl.gz", destfile = "gene2ensembl.gz")
 
-gene2ensembl <- read.delim("~/Library/CloudStorage/iCloudDrive/Desktop/all_human_genes/gene2ensembl.gz", header=TRUE)
+gene2ensembl <- read.delim("gene2ensembl.gz", header=TRUE)
 
 head(gene2ensembl %>% filter(X.tax_id == 9606))
 
@@ -12,4 +12,4 @@ human_protein_coding_gene <- gene2ensembl %>%
   select(GeneID, Ensembl_gene_identifier) %>%
   unique()
 
-write.table(human_protein_coding_gene, file="/Users/yichengtsai/Desktop/all_human_genes/human_protein_coding_genes.tsv", sep='\t', row.names=FALSE, quote=FALSE)
+write.table(human_protein_coding_gene, file="human_protein_coding_genes.tsv", sep='\t', row.names=FALSE, quote=FALSE)
