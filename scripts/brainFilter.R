c5b973f36a3e1b13ed3ed5654e56e1cbf9cd2be0
David Guo <itsDagu@users.noreply.github.com>
2019-10-19 15:36:22 -0700

    Day 2 Deliverables
    (almost)Fully automated pipeline

--- scripts/brainFilter.R
+++ scripts/brainFilter.R
@@ -8,19 +8,15 @@ pcgenes <- data.frame(read.csv(file = "../reference/human_protein_coding_genes.t
 for (i in 1:length(brainfile)){
   name <- brainreg[i]
   file <- brainfile[i]
-  genelist <- read.csv(file, header = TRUE, row.names = 1) 
-
-  # create filtered list of genes (protein coding only)
-  (index <- which(rownames(genelist) %in% pcgenes$Ensembl_gene_identifier))
-  filterlist <- genelist[index,]
-  assign(name, filterlist)
-
+  genelist <- read.csv(file, header = TRUE, row.names = 1)
+  #assign(name, genelist)
+  
   # create table with mean values of gene expression (in cpm) for each region
   if (i==1){
-    brainmean <- data.frame(rowMeans(filterlist))
+    brainmean <- data.frame(rowMeans(genelist))
   }
   else {
-    newdf <- data.frame(rowMeans(filterlist))
+    newdf <- data.frame(rowMeans(genelist))
     brainmean <- as.data.frame(cbind(brainmean, newdf))
   }
   
@@ -28,6 +24,18 @@ for (i in 1:length(brainfile)){
     colnames(brainmean) <- brainreg
   }
 }
+
+# create filtered list of genes (protein coding only)
+library(dplyr)
+brainmean$ensembl_id <- gsub("\\..*","",rownames(brainmean))
+brainmean <- distinct(.data = brainmean, ensembl_id,.keep_all = TRUE)
+dim(brainmean)
+
+rownames(brainmean) <- brainmean$ensembl_id
+brainmean <- brainmean[,1:ncol(brainmean)-1]
+
+(index <- which(rownames(brainmean) %in% pcgenes$Ensembl_gene_identifier))
+brainmean <- brainmean[index,]
 
 brainmean$max <- apply(brainmean[,], 1, max)
 
@@ -43,13 +51,13 @@ uniprotid <- (gsub("UniProtKB:", "",uniprotid))
 mart.hs <- useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl")
 geneid <- getBM(attributes=c("uniprot_gn_id", "ensembl_gene_id", "external_gene_name"), filter="uniprot_gn_id", values = uniprotid, mart=mart.hs)
 
-## FILTER BRAIN DATA FOR SURFACE MOLECULES
-brainsurf <- brainmean[rownames(brainmean) %in% geneid$ensembl_gene_id,]
-
-write.csv(brainsurf, file = "../outputs/brain_surf_counts.csv", quote = FALSE)
-
+# ## FILTER BRAIN DATA FOR SURFACE MOLECULES
+# brainsurf <- brainmean[rownames(brainmean) %in% geneid$ensembl_gene_id,]
+# 
+# write.csv(brainsurf, file = "../outputs/brain_surf_counts.csv", quote = FALSE)
+brainsurf <- brainmean
 # LOAD PPI PAIRS
-ppilist <- read.csv(file = "../reference/human-PPI-pairs.csv", header = TRUE)
+ppilist <- read.csv(file = "../reference/human_surface_pp_interaction.csv", header = TRUE)
 ppicombA <- matrix(ppilist[, "ENSEMBL_A"])
 ppicombB <- matrix(ppilist[, "ENSEMBL_B"])
 ppicomb <- rbind(ppicombA,ppicombB); ppicomb
@@ -57,24 +65,47 @@ ppicomb <- rbind(ppicombA,ppicombB); ppicomb
 #FILTER DATA FOR PPI PAIRS AND GENERATE TWO MATRICES (EACH GENE IN PAIR)
 brainppiA <- brainsurf[0,]
 brainppiB <- brainsurf[0,]
+
+brainsurf$ensembl_id <- rownames(brainsurf)
+rownames(brainsurf) <- NULL
 for (i in 1:nrow(ppilist)) {
-  if((ppicombA[i] %in% rownames(brainsurf))&(ppicombB[i] %in% rownames(brainsurf))) {
-    indexa <- which(rownames(brainsurf) %in% ppicombA[i])
-    print(indexa)
-    indexb <- which(rownames(brainsurf) %in% ppicombB[i])
-    print(indexb)
+  if((ppicombA[i] %in% brainsurf$ensembl_id)&(ppicombB[i] %in% brainsurf$ensembl_id)) {
+    indexa <- which(brainsurf$ensembl_id %in% ppicombA[i])
+    indexb <- which(brainsurf$ensembl_id %in% ppicombB[i])
+    
     brainppiA <- rbind(brainppiA, brainsurf[indexa,])
     brainppiB <- rbind(brainppiB, brainsurf[indexb,])
   }
 }
+
+brainppiA <- brainppiA[ ,c(ncol(brainppiA), 1:ncol(brainppiA)-1)]
+brainppiB <- brainppiB[ ,c(ncol(brainppiB), 1:ncol(brainppiB)-1)]
+
 write.csv(brainppiA, file = "../outputs/brain_ppiA_counts.csv", quote = FALSE)
 write.csv(brainppiB, file = "../outputs/brain_ppiB_counts.csv", quote = FALSE)
 
 # CREATE MERGE MATRIX WITH BOTH GENE NAMES AND THE MINIMUM OF THE MAX REGION MEAN COUNTS
 minppi <- data.frame("Amax"=brainppiA$max, "Bmax"=brainppiB$max); minppi
-brainppi <- data.frame("GeneA" = rownames(brainppiA),
-                       "GeneB" = rownames(brainppiB),
+
+for (i in 1:nrow(brainppiA)){
+  if (brainppiA$ensembl_id[i] %in% geneid$ensembl_gene_id) {
+    brainppiA$genename[i] <- geneid$external_gene_name[which(geneid$ensembl_gene_id %in% brainppiA$ensembl_id[i])]
+  }
+  else {
+    brainppiA$genename[i] <- NA
+  }
+  if (brainppiB$ensembl_id[i] %in% geneid$ensembl_gene_id) {
+    brainppiB$genename[i] <- geneid$external_gene_name[which(geneid$ensembl_gene_id %in% brainppiB$ensembl_id[i])]
+  }
+  else {
+    brainppiB$genename[i] <- NA
+  }
+}
+
+brainppi <- data.frame("Ensembl_GeneID_A" = brainppiA$ensembl_id,
+                       "Ensembl_GeneID_B" = brainppiB$ensembl_id,
+                       "GeneA" = brainppiA$genename,
+                       "GeneB" = brainppiB$genename,
                        "min" = apply(minppi, 1, max))
 
 write.csv(brainppi, file = "../outputs/brain_ppi_mapping.csv", quote = FALSE, row.names = FALSE)
-
--- scripts/immunoFilter.R
+++ scripts/immunoFilter.R
@@ -1,26 +1,112 @@
-col.data.path <- "/home/team9_hs19/immunoData/immunoColData.csv"
-gene.counts.path <- "/home/team9_hs19/immunoData/immunoGeneCounts.csv"
+## Set your working directory as SOURCE FILE location (Session -> Set Working Directory -> Source File Location)
+immunofile <- list.files(path="../immunodata/", pattern = "_TPM.csv", full.names = TRUE); immunofile
+immunoreg <- gsub(list.files(path="../immunodata/", pattern = "_TPM.csv"), pattern = "_TPM.csv",replacement = ""); immunoreg
 
-# reading the annotations file
-annotations <- read.csv(file=col.data.path, skip=1, header=FALSE, sep=",")
-srr.to.celltype <- annotations[, c(1, 23)]
+## FILTER BY PROTEIN CODING GENES
+pcgenes <- data.frame(read.csv(file = "../reference/human_protein_coding_genes.tsv", header = TRUE, sep = "\t"))
 
-# indexing based on the rownames
-rownames(srr.to.celltype) <- annotations[, 1]
+for (i in 1:length(immunofile)){
+  name <- immunoreg[i]
+  file <- immunofile[i]
+  genelist <- read.csv(file, header = TRUE, row.names = 1)
+  genelist <- genelist[3:ncol(genelist)]
+  #assign(name, genelist)
+  
+  # create table with mean values of gene expression (in cpm) for each region
+  if (i==1){
+    immunomean <- data.frame(rowMeans(genelist))
+  }
+  else {
+    newdf <- data.frame(rowMeans(genelist))
+    immunomean <- as.data.frame(cbind(immunomean, newdf))
+  }
+  
+  if (i==length(immunofile)){
+    colnames(immunomean) <- immunoreg
+  }
+}
 
-# reading and indexing the gene counts
-gene.counts <- read.csv(file=gene.counts.path, header=TRUE, sep=",", row.names=1)
-cell.type.names <- make.names(srr.to.celltype[colnames(gene.counts), ]$V23)
-colnames(gene.counts) <- cell.type.names
+# create filtered list of genes (protein coding only)
+library(dplyr)
+immunomean$ensembl_id <- gsub("\\..*","",rownames(immunomean))
+immunomean <- distinct(.data = immunomean, ensembl_id,.keep_all = TRUE)
+dim(immunomean)
 
-# aggregate the counts based on the cell type
-aggregate <- t(rowsum(t(gene.counts), group=cell.type.names))
-freqs <- table(cell.type.names)
+rownames(immunomean) <- immunomean$ensembl_id
+immunomean <- immunomean[,1:ncol(immunomean)-1]
 
-# Extracting mean of the counts
-mean <- data.frame(matrix(mapply('/', aggregate, freqs),  ncol=length(freqs)))
-rownames(mean) <- rownames(aggregate)
-colnames(mean) <- colnames(aggregate)
-mean$colMax <- apply(t(mean), 2, function(x) max(x) )
+(index <- which(rownames(immunomean) %in% pcgenes$Ensembl_gene_identifier))
+immunomean <- immunomean[index,]
 
-write.csv(mean, file="immuno_mean_counts.csv",quote = FALSE)
+immunomean$max <- apply(immunomean[,], 1, max)
+
+write.csv(immunomean, file = "../outputs/immuno_mean_counts.csv", quote = FALSE)
+
+## CONVERT SURFACE MOLECULES FROM UNIPROT TO ENSEMBL
+library(biomaRt)
+
+surflist <- as.matrix(read.csv(file = "../reference/Hs_cell-surface-671.csv", header = TRUE)[,1])
+uniprotid <- surflist[grep("UniProtKB:",surflist)]
+uniprotid <- (gsub("UniProtKB:", "",uniprotid))
+
+mart.hs <- useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl")
+geneid <- getBM(attributes=c("uniprot_gn_id", "ensembl_gene_id_version", "ensembl_gene_id", "external_gene_name"), filter="uniprot_gn_id", values = uniprotid, mart=mart.hs)
+
+## FILTER immuno DATA FOR SURFACE MOLECULES
+immunosurf <- immunomean[rownames(immunomean) %in% geneid$ensembl_gene_id,]
+
+write.csv(immunosurf, file = "../outputs/immuno_surf_counts.csv", quote = FALSE)
+
+# LOAD PPI PAIRS
+ppilist <- read.csv(file = "../reference/biogrid_table_pp_interaction.csv", header = TRUE)
+ppicombA <- matrix(ppilist[, "ENSEMBL_A"])
+ppicombB <- matrix(ppilist[, "ENSEMBL_B"])
+ppicomb <- rbind(ppicombA,ppicombB); ppicomb
+
+#FILTER DATA FOR PPI PAIRS AND GENERATE TWO MATRICES (EACH GENE IN PAIR)
+immunoppiA <- immunosurf[0,]
+immunoppiB <- immunosurf[0,]
+
+immunosurf$ensembl_id <- rownames(immunosurf)
+rownames(immunosurf) <- NULL
+for (i in 1:nrow(ppilist)) {
+  if((ppicombA[i] %in% immunosurf$ensembl_id)&(ppicombB[i] %in% immunosurf$ensembl_id)) {
+    indexa <- which(immunosurf$ensembl_id %in% ppicombA[i])
+    indexb <- which(immunosurf$ensembl_id %in% ppicombB[i])
+    
+    immunoppiA <- rbind(immunoppiA, immunosurf[indexa,])
+    immunoppiB <- rbind(immunoppiB, immunosurf[indexb,])
+  }
+}
+
+immunoppiA <- immunoppiA[ ,c(ncol(immunoppiA), 1:ncol(immunoppiA)-1)]
+immunoppiB <- immunoppiB[ ,c(ncol(immunoppiB), 1:ncol(immunoppiB)-1)]
+
+write.csv(immunoppiA, file = "../outputs/immuno_ppiA_counts.csv", quote = FALSE)
+write.csv(immunoppiB, file = "../outputs/immuno_ppiB_counts.csv", quote = FALSE)
+
+# CREATE MERGE MATRIX WITH BOTH GENE NAMES AND THE MINIMUM OF THE MAX REGION MEAN COUNTS
+minppi <- data.frame("Amax"=immunoppiA$max, "Bmax"=immunoppiB$max); minppi
+
+for (i in 1:nrow(immunoppiA)){
+  if (immunoppiA$ensembl_id[i] %in% geneid$ensembl_gene_id) {
+    immunoppiA$genename[i] <- geneid$external_gene_name[which(geneid$ensembl_gene_id %in% immunoppiA$ensembl_id[i])]
+  }
+  else {
+    immunoppiA$genename[i] <- NA
+  }
+  if (immunoppiB$ensembl_id[i] %in% geneid$ensembl_gene_id) {
+    immunoppiB$genename[i] <- geneid$external_gene_name[which(geneid$ensembl_gene_id %in% immunoppiB$ensembl_id[i])]
+  }
+  else {
+    immunoppiB$genename[i] <- NA
+  }
+}
+
+immunoppi <- data.frame("Ensembl_GeneID_A" = immunoppiA$ensembl_id,
+                       "Ensembl_GeneID_B" = immunoppiB$ensembl_id,
+                       "GeneA" = immunoppiA$genename,
+                       "GeneB" = immunoppiB$genename,
+                       "min" = apply(minppi, 1, max))
+
+write.csv(immunoppi, file = "../outputs/immuno_ppi_mapping.csv", quote = FALSE, row.names = FALSE)
