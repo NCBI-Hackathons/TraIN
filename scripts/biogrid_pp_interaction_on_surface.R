7e5c5489c84d99d3794d0fd1b737583c04277964
almas2019 <47234185+almas2019@users.noreply.github.com>
2019-10-19 14:54:44 -0700

    added comments to file

--- scripts/biogrid_pp_interaction_on_surface.R
+++ scripts/biogrid_pp_interaction_on_surface.R
@@ -9,25 +9,33 @@ Biogrid <- unzip(here("BIOGRID-ALL-3.5.177.tab2.zip"))
 
 Biogrid_table <- read.delim(Biogrid)
 
+## Filter for human
 Biogrid_table <- Biogrid_table %>%
   filter(Organism.Interactor.A==9606,Organism.Interactor.B==9606)
 
 human_protein_coding_genes <- read.delim(here("human_protein_coding_genes.tsv"))
 human_protein_coding_genes$GeneID <- as_factor(human_protein_coding_genes$GeneID)
+
+## Rename the protein coding genes reference
 human_prot_gene_A <- dplyr:::select(human_protein_coding_genes,Entrez.Gene.Interactor.A=GeneID,
                                     ENSEMBL_A=Ensembl_gene_identifier)
 
 human_prot_gene_B <- dplyr:::select(human_protein_coding_genes,Entrez.Gene.Interactor.B=GeneID,
                                 ENSEMBL_B=Ensembl_gene_identifier) 
 
+# Join tables 
 Biogrid_table <- Biogrid_table %>%
   left_join(.,human_prot_gene_A)
 Biogrid_table <- Biogrid_table %>%
   left_join(.,human_prot_gene_B)
 
+## Download surface genes
 surface_genes <- read.delim(here("human_surface_genes.tsv"))
+
+#Filter for surface genes
 Biogrid_table <- Biogrid_table %>%
   filter( ENSEMBL_A %in% surface_genes$Ensembl_gene_identifier &
             ENSEMBL_B %in% surface_genes$Ensembl_gene_identifier)
 
+# Write to file
 write_csv(Biogrid_table,here("human_surface_pp_interaction.csv"))
